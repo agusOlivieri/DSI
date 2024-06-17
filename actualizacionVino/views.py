eb587a6106d1aeae806f841fa0725c809e479b74
Thiago Castoldi <castoldithiago4@gmail.com>
2024-06-17 17:54:02 -0300

    ultimo diagrama

--- actualizacionVino/views.py
+++ actualizacionVino/views.py
@@ -21,15 +21,15 @@ class PantallaImportarActualizaciones:
             'bodegas': bodegasParaActualizar
         })
     
-    def tomarSeleccionBodega(request, nombre):
-        return GestorImportarActualizaciones.tomarSeleccionBodega(request, nombre)
+    def tomarSeleccionBodega(request, nombreBodega):
+        return GestorImportarActualizaciones.tomarSeleccionBodega(request, nombreBodega)
         
         # return PantallaImportarActualizaciones.mostrarResumenVinosImportados(request, actualizaciones, nombre)
         
 
-    def mostrarResumenVinosImportados(request, actualizaciones, nombreBod, notif):
+    def mostrarResumenVinosImportados(request, vinosImportados, nombreBod, notif):
         return render(request, 'vinos_importados.html', {
-            'actualizaciones': actualizaciones,
+            'vinosImportados': vinosImportados,
             'bodega': nombreBod,
             'notificacion': notif
         })
@@ -39,59 +39,3 @@ class PantallaImportarActualizaciones:
 
 def index(request): 
     return render(request, 'index.html')
-
-
-###############
-# def verActualizaciones(request):
-#     with open("actualizaciones.json") as file:
-#         data = json.load(file)
-#     return render(request, 'ver_actualizaciones.html', {
-#         'json': data
-#     })
-
-# def actualizarVino(request, id):
-#     with open("actualizaciones.json") as file:
-#         data = json.load(file)
-    
-#     actualizaciones = data["bodegas"][id-1]["actualizaciones"]
-
-#     listaVinos = []
-
-#     for a in actualizaciones:
-#         if (a["crear"] == False):
-#             vinoId = a["id"]
-#             precio = a["precioARS"]
-#             nota = a["NotaDeCata"]
-#             imagen = a["ImagenEtiqueta"]
-
-#             vino = Vino.objects.get(id=vinoId)
-#             vino.precioARS = precio
-#             vino.notaDeCata = nota
-#             vino.ImagenEstiqueta = imagen
-#             vino.save()
-#             listaVinos.append(vino)
-
-#         elif (a["crear"] == True):
-#             maridajeId = a["maridaje"]
-#             varietalId = a["varietal"]
-#             m = Maridaje.objects.get(id=maridajeId)
-#             v = Varietal.objects.get(id=varietalId)
-#             b = Bodega.objects.get(id=id)
-            
-#             aniada = a["aniada"]
-#             imagen = a["ImagenEtiqueta"]
-#             nota = a["NotaDeCata"]
-#             precio = a["precioARS"]
-#             nom = a["nombre"]
-
-#             vino = Vino(añada=aniada, ImagenEstiqueta=imagen, nombre=nom, notaDeCata=nota, precioARS=precio, maridaje=m, varietal=v, bodega=b)
-#             vino.save()
-#             listaVinos.append(vino)
-
-#     bodega = Bodega.objects.get(id=id)
-#     print("")
-#     return render(request, 'lista_de_vinos.html', {
-#         "vinos": listaVinos,
-#         "bodega": bodega
-#     })
-###############
